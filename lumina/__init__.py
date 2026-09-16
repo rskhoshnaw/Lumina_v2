86b5e82689726fe208979461f7676082d8a440e3
Rasul khoshnaw <129181423+rskhoshnaw@users.noreply.github.com>
2026-09-16 09:16:12 +0300

    Update __init__.py

--- lumina/__init__.py
+++ lumina/__init__.py
@@ -1,1 +1,10 @@
+"""Lumina Animation Engine public API."""
 
+from .cards import FormulaHighlightCard, QuoteCard
+from .scene import LuminaScene
+
+__all__ = [
+    "LuminaScene",
+    "QuoteCard",
+    "FormulaHighlightCard",
+]
